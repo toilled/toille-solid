611f8d8778778bae496b9f80f608e7373ac1ce00
Elliot Dickerson <elliot@toille.uk>
2023-07-06 14:59:01 +0100

    Title bounce

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,13 +1,28 @@
-import { Component } from "solid-js";
+import { Component, createSignal } from "solid-js";
 import Titles from "../interfaces/Titles";
 
 const Title: Component<Titles> = (props: Titles) => {
+  const [titleAnimate, setTitleAnimate] = createSignal(false);
+  const [subtitleAnimate, setSubtitleAnimate] = createSignal(false);
+
   return (
     <ul class="animate__animated animate__slideInLeft">
       <li>
         <hgroup>
-          <h1>{props.title}</h1>
-          <h2>{props.heading}</h2>
+          <h1
+            classList={{
+              animate__animated: true,
+              animate__heartBeat: titleAnimate() === true
+            }}
+            onclick={() => { setTitleAnimate(true); setTimeout(() => setTitleAnimate(false), 1000) }}
+          >{props.title}</h1>
+          <h2
+            classList={{
+              animate__animated: true,
+              animate__heartBeat: subtitleAnimate() === true
+            }}
+            onclick={() => { setSubtitleAnimate(true); setTimeout(() => setSubtitleAnimate(false), 1000) }}
+          >{props.heading}</h2>
         </hgroup>
       </li>
     </ul>
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,13 @@ import { render } from 'solid-js/web';
 
 import './index.css';
 import App from './App';
-import { Show, Suspense, createResource } from 'solid-js';
+import { Show, createResource } from 'solid-js';
 
 const root = document.getElementById('root');
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error(
-    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
+    'Root element not found.',
   );
 }
 
