7ed46131e564652efe8a97315c138dd2c57dcb6d
Elliot Dickerson <elliot@toille.uk>
2023-08-07 13:37:54 +0100

    Moved configs from API into files

--- src/index.tsx
+++ src/index.tsx
@@ -5,26 +5,18 @@ import 'animate.css';
 
 import './index.css';
 import App from './App';
-import { Show, createResource } from 'solid-js';
+import {createSignal} from 'solid-js';
+
+import pagesConfig from './configs/pages.json';
+import titlesConfig from './configs/titles.json';
 
 const root = document.getElementById('root');
 
-if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
-  throw new Error(
-    'Root element not found.',
-  );
-}
-
-const fetchTitles = async () => (await fetch('http://toille.uk/api/titles')).json();
-const [titles] = createResource(fetchTitles);
-
-const fetchPages = async () => (await fetch('http://toille.uk/api/pages')).json();
-const [pages] = createResource(fetchPages);
+const [titles] = createSignal(titlesConfig);
+const [pages] = createSignal(pagesConfig);
 
 render(() => (
   <>
-    <Show when={titles() && pages()} fallback={<h1 class="animate__animated animate__zoomIn">Loading...</h1>}>
       <App titles={titles()} pages={pages()} />
-    </Show>
   </>
 ), root!);
