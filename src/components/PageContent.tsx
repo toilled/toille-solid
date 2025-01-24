a3dbde49df37d3b31c17b041e236a040811d42a5
Elliot Dickerson <elliot@toille.uk>
2025-01-24 16:15:41 +0000

    Update PageContent hint to use solid-transition-group as well

--- src/components/PageContent.tsx
+++ src/components/PageContent.tsx
@@ -1,6 +1,7 @@
 import { Component, For, Show, createSignal } from "solid-js";
 import { Page } from "../interfaces/Page";
 import { Paragraph } from "./Paragraph";
+import { Transition } from "solid-transition-group";
 
 interface PageProps {
   page: () => Page
@@ -18,6 +19,19 @@ export const PageContent: Component<PageProps> = ({ page }: PageProps) => {
     title: true,
   };
 
+  const fadeIn = (el: Element, done: () => void): void => {
+    const a = el.animate([{ opacity: 0 }, { opacity: 1 }], {
+      duration: 500
+    });
+    a.finished.then(done);
+  };
+  const fadeOut = (el: Element, done: () => void): void => {
+    const a = el.animate([{ opacity: 1 }, { opacity: 0 }], {
+      duration: 500
+    });
+    a.finished.then(done);
+  };
+
   return (
     <main classList={contentClasses}>
       <header>
@@ -30,17 +44,16 @@ export const PageContent: Component<PageProps> = ({ page }: PageProps) => {
             }, 500);
           }}
         >{page().title}
-          <Show when={showHint()}>
-            <span
-              classList={{ animate__animated: true, animate__zoomIn: true, }}
-              style="font-weight: 100;font-style: italic;font-size:0.6em;vertical-align: middle;"
-            > - Nothing here</span>
-          </Show>
+          <Transition onEnter={fadeIn} onExit={fadeOut} >
+            <Show when={showHint()}>
+              <span style="font-weight: 100;font-style: italic;font-size:0.6em;vertical-align: middle;"> - Nothing here</span>
+            </Show>
+          </Transition>
         </h2>
       </header>
       <For each={page().body}>{
         (paragraph, index) => <Paragraph paragraph={paragraph} last={index() + 1 === page().body.length} />
       }</For>
-    </main>
+    </main >
   );
 };
