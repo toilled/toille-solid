04e05321fd08434cfcd58f5b3b82d5d3eb05e7bb
toilled <elliot@toille.uk>
2025-03-06 13:33:41 +0000

    Faster transitions

--- src/transitions/fades.ts
+++ src/transitions/fades.ts
@@ -1,12 +1,14 @@
+const duration = 250;
+
 export const fadeIn = (el: Element, done: () => void): void => {
   const a = el.animate([{ opacity: 0 }, { opacity: 1 }], {
-    duration: 600,
+    duration: duration,
   });
   a.finished.then(done);
 };
 export const fadeOut = (el: Element, done: () => void): void => {
   const a = el.animate([{ opacity: 1 }, { opacity: 0 }], {
-    duration: 600,
+    duration: duration,
   });
   a.finished.then(done);
 };
